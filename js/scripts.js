c158246f1d08dd74b92b423e356c9ceb8e8cf5f5
ura <byorickq@gmail.com>
2014-12-03 21:17:48 +0200

    calc

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,37 @@ jQuery(document).ready(function($) {
     $(".language-current").removeClass('active');
   });
   /*-------languages-end-------*/
+
+
+  /*----------Stick-price-start-------------*/
+  $(window).scroll(function(event) {
+    calc = $(".calc");
+    calc_price = $(".calc-price");
+    if (calc.length !== 0) {
+    offset = calc.offset().top;
+    height = calc.height() - 30;
+    height_price = calc_price.height();
+    if ($(window).scrollTop()>offset) {
+      calc_price.css('top', $(window).scrollTop()-offset);
+    };
+    if ($(window).scrollTop()<offset) {
+      calc_price.css('top', 0);
+    };
+    if ($(window).scrollTop()>offset+height-height_price) {
+      calc_price.css('top', height-height_price);
+    };
+    };
+  });
+  /*----------Stick-price-end-------------*/
+
+  $(".expand").click(function(event) {
+    $(".hided").removeClass('hided');
+    $(".short-description").addClass('hided');
+    return false;
+
+  });
+  /*----------Expand-more-------------*/
+
+  /*----------Expand-more-end-------------*/
+
 });
